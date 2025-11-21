267e6a8f8f8bee51f3ea8cafee093b045defa97d
sherix <mrhyder29@gmail.com>
2025-11-21 14:19:00 +0500

    abstract classes

---
+++ abstractClass.php
@@ -0,0 +1,38 @@
+<?php
+
+abstract class Shape{
+    abstract public function getArea();
+    public function describe(){
+        echo "This is a shape.";
+    }
+}
+class Circle extends Shape{
+    public $radius;
+    public function __construct($radius){
+        $this->radius = $radius;
+    }
+    public function getArea(){
+        return 3.14*$this->radius*$this->radius;
+    }
+}
+class rectangle extends Shape{
+    public $height;
+    public $width;
+    public function __construct($height,$width){
+        $this->height = $height;
+        $this->width = $width;
+    }
+    public function getArea(){
+        return $this->height*$this->width;
+    }
+}
+
+$c1 = new Circle(5);
+echo "the area of the circle is {$c1->getArea()} <br>";
+$r1 = new rectangle(4,5);
+echo "the area of the rectangle is {$r1->getArea()}<br>";
+
+$c1->describe();
+$r1->describe();
+
+?>
